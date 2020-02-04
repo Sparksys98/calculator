ea10879cd5b0e58e02917d5b42f949d30bbe3eba
sparksys98 <hanineiroukh@gmail.com>
2020-02-04 15:30:23 +0200

    Calculator

--- calculator.py
+++ calculator.py
@@ -1,8 +1,32 @@
-
+import sys
 def main():
-	#write your code here
+	num1=input("Enter your first number: ")
+	num2=input("Enter your second number: ")
+	char=input("Choose the operation: ")
+
+	if num1.isdigit()==False or num2.isdigit()==False:
+		print("only numbers please")
+		
+
+	if num1.isdigit()==True and num2.isdigit()==True and char=='*':
+			num1=int(num1)
+			num2=int(num2)
+			print (num1*num2)
+	elif num1.isdigit()==True and num2.isdigit()==True and char=='+':
+			num1=int(num1)
+			num2=int(num2)
+			print (num1+num2)
+	elif num1.isdigit()==True and num2.isdigit()==True and char=='-':
+			num1=int(num1)
+			num2=int(num2)
+			print (num1-num2)
+	elif num1.isdigit()==True and num2.isdigit()==True and char=='/':
+			num1=int(num1)
+			num2=int(num2)
+			print (num1/num2)
+
 	pass
-	
+
 
 
 
